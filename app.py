eadd037b75abb404cbe699481b468dbb27527137
Xinge Tong <139126979+xingetong@users.noreply.github.com>
2025-09-15 14:39:40 +0100

    app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ from langchain.llms import HuggingFacePipeline
 import streamlit as st
 
 from huggingface_hub import login
-login(token="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")
+login(token="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")
 
 
 def parse_time_column(df, time_col='time'):
